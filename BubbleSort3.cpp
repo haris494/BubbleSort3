c129ab2cf8fae51d1d0d8759f9880dffaccfc945
haris494 <hariis.459@gmail.com>
2023-03-16 19:28:51 +0700

    Add project files.

---
+++ BubbleSort3.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#using namespace std; 
+
+
+int a[20];
+int n)
+
+
+void input() (
+    while (true) (
+        cout << "Masukan banyaknya elemen pada array : ")
+        cin >> n;
+        if (n <= 20)
+        break;
+        else (
+            cout << "nArray dapat mempunyai 20 maksimal.\n";
+        )
+    )
+    cout << end];
+    cout << "====================" << end];
+    cout << "Masukkan elemen array" << end];
+    cout << "====================" << end];
+    for (int i = 1; i < n; i++) {
+        cout << a[j] << end1;
+    )
+ )
+
+ void bubbleSortArray() {
+     for (int i = 1; i < n; i++) {
+       for (int i = 1; j < n - i; j++) {
+           if (a[j] > a[j + 1]) (
+              int(temp = a[j];
+              a[j] = a[j + 1];
+              a[j + 1] = temp)
+            )
+         )
+      )
+ )
+
+
+ int main() (
+     input();
+     bubbleSortArray();
+     display();
+     return 0;
+)
